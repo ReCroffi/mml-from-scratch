672755f7b06c3adb571a63998743d0f9d9baf69f
ReCroffi <renancroffi@gmail.com>
2026-07-13 16:51:49 -0300

    test: regressão sobre scores do PCA == sobre features (invariância a rotação)

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,6 +1,7 @@
 from src.regression import gradient_descent, normal_equation, mse_loss, gradient
+from src.pca import normalize, PCA
 import numpy as np
-
+from sklearn.datasets import load_wine
 
 def test_gd_converge_pra_normal_equation():
     rng = np.random.default_rng(42)
@@ -26,4 +27,14 @@ def test_gradient_bate_com_numerico():
         grad_num[i] = (mse_loss(X, y, w=w_mais) - mse_loss(X, y, w=w_menos))/(2*eps)
     assert np.allclose(grad_analitico, grad_num)
     
-        
+def test_regressao_nos_scores_pca():
+    wine = load_wine()
+    y = wine.data[:,12]
+    X = wine.data[:, :12]
+    Xn, _, _ = normalize(X)
+    score, _, _, _, _ = PCA(X, num_components = X.shape[1])
+    w_feat = normal_equation(Xn, y)
+    mse_feat = mse_loss(Xn, y, w_feat)
+    w_score = normal_equation(score, y)
+    mse_score = mse_loss(score, y, w_score)
+    assert np.allclose(mse_feat, mse_score)
